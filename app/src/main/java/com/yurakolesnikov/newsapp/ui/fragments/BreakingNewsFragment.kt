6c9c3304d7623db3da0d747ac7dacb09bc701630
Yura <yurikolesnicov@gmail.com>
2022-02-07 19:19:24 +0300

    Bugs fixed in BreakingNewsFragment. Now works perfectly fine.

--- app/src/main/java/com/yurakolesnikov/newsapp/ui/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/com/yurakolesnikov/newsapp/ui/fragments/BreakingNewsFragment.kt
@@ -17,6 +17,7 @@ import com.yurakolesnikov.newsapp.databinding.FragmentBreakingNewsBinding
 import com.yurakolesnikov.newsapp.ui.NewsViewModel
 import com.yurakolesnikov.newsapp.ui.NewsActivity
 import com.yurakolesnikov.newsapp.utils.AutoClearedValue
+import com.yurakolesnikov.newsapp.utils.Constants.Companion.QUERY_PAGE_SIZE
 import com.yurakolesnikov.newsapp.utils.Resource
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.MainScope
@@ -32,7 +33,6 @@ class BreakingNewsFragment : Fragment() {
     lateinit var newsAdapter: NewsAdapter
 
     var isLoading = false
-
 
 
     val TAG = "BreakingNewsFragment"
@@ -58,7 +58,7 @@ class BreakingNewsFragment : Fragment() {
         }
 
         newsAdapter.differ.submitList(viewModel.breakingNewsResponse?.articles)
-        if (viewModel.breakingNewsResponse == null) viewModel.getBreakingNews("us")
+        //if (viewModel.breakingNewsResponse == null) viewModel.getBreakingNews("us")
 
         viewModel.breakingNews.observe(viewLifecycleOwner, Observer { response ->
             when (response) {
@@ -66,6 +66,9 @@ class BreakingNewsFragment : Fragment() {
                     hideProgressBar()
                     response.data?.let { newsResponse ->
                         newsAdapter.differ.submitList(newsResponse.articles.toList())
+                        val rangeStart = (viewModel.breakingNewsPage - 2) * QUERY_PAGE_SIZE + 1
+                        val itemCount = newsResponse.articles.size - rangeStart + 1
+                        newsAdapter.notifyItemRangeInserted(rangeStart, itemCount)
                         viewModel.totalResults = newsResponse.totalResults
                     }
                 }
@@ -96,20 +99,11 @@ class BreakingNewsFragment : Fragment() {
         override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
             super.onScrollStateChanged(recyclerView, newState)
 
-            // If connection appear, all we need to refresh page is to swipe screen
-            if (!viewModel.previousInternetStateBreakingNews && viewModel.hasInternetConnection()) {
-                viewModel.getBreakingNews("us")
-            } else viewModel.breakingNews.postValue(Resource.Error("no internet connection"))
-        }
-
-        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-            super.onScrolled(recyclerView, dx, dy)
-
             val layoutManager = recyclerView.layoutManager as LinearLayoutManager
-            val lastVisibleItemPosition = layoutManager.findLastCompletelyVisibleItemPosition()
+            val lastVisibleItemPosition = layoutManager.findLastVisibleItemPosition()
             val totalItemCountInAdapter = layoutManager.itemCount
 
-            val isAtLastItem = (lastVisibleItemPosition + 2) == totalItemCountInAdapter
+            val isAtLastItem = (lastVisibleItemPosition + 1) >= totalItemCountInAdapter
             val isTotalMoreThanVisible = viewModel.totalResults ?: 0 > totalItemCountInAdapter
 
             val shouldPaginate = isAtLastItem && isTotalMoreThanVisible && !isLoading
@@ -117,6 +111,25 @@ class BreakingNewsFragment : Fragment() {
             if (shouldPaginate) {
                 viewModel.getBreakingNews("us")
             }
+
+            // If connection appear, all we need to refresh page is to swipe screen
+            if (viewModel.breakingNewsResponse == null
+                && !viewModel.previousInternetStateBreakingNews
+                && viewModel.hasInternetConnection()
+            ) {
+                viewModel.getBreakingNews("us")
+            }
+            when (viewModel.breakingNewsResponse) {
+                null -> if (!viewModel.hasInternetConnection())
+                    viewModel.breakingNews.postValue(Resource.Error("no internet connection"))
+                else -> if (!viewModel.hasInternetConnection() && isAtLastItem)
+                    viewModel.breakingNews.postValue(Resource.Error("no internet connection"))
+            }
+
+        }
+
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
         }
     }
 
